e8acc40757cd2255af556399072b589348b69663
Himanshu116k <himanshu1991patna@gmail.com>
2026-03-16 21:11:11 +0530

    second commit for this db connection establishment

---
+++ src/main/java/com/himanshu/quizapp/QuestionController.java
@@ -0,0 +1,16 @@
+package com.himanshu.quizapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @GetMapping("allQuestions")
+    public String getAllQuestions(){
+      return "HI just for testing purpose for all questions";
+    }
+
+}
