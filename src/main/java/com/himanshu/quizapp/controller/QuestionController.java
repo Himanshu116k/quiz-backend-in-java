5d37609995939695a42dadcb93380dc1414f7739
Himanshu116k <himanshu1991patna@gmail.com>
2026-03-17 21:29:03 +0530

    adding add Ques Route

--- src/main/java/com/himanshu/quizapp/controller/QuestionController.java
+++ src/main/java/com/himanshu/quizapp/controller/QuestionController.java
@@ -3,10 +3,7 @@ package com.himanshu.quizapp.controller;
 import com.himanshu.quizapp.Question;
 import com.himanshu.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,12 +19,14 @@ public class QuestionController {
     }
 
     @GetMapping("category/{category}")
-    public List<Question> getQustionByCategory(@PathVariable String category){
+    public List<Question> getQustionByCategory(@PathVariable String category) {
         return questionService.getQuestionsByCategory(category);
+    }
 
 
-
-
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question){
+        return questionService.addQuestion(question);
     }
 
 }
--- src/main/java/com/himanshu/quizapp/service/QuestionService.java
+++ src/main/java/com/himanshu/quizapp/service/QuestionService.java
@@ -18,4 +18,9 @@ public class QuestionService {
     public List<Question> getQuestionsByCategory(String category) {
         return  questionDao.findByCategory(category);
     }
+
+    public String addQuestion(Question question) {
+        questionDao.save(question);
+        return "Success";
+    }
 }
