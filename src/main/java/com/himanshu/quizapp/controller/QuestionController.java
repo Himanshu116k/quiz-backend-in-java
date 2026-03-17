ae6a12a9b1e344c7dfa0012ffa020170ae02da37
Himanshu116k <himanshu1991patna@gmail.com>
2026-03-17 20:55:19 +0530

    Adding category route

--- src/main/java/com/himanshu/quizapp/controller/QuestionController.java
+++ src/main/java/com/himanshu/quizapp/controller/QuestionController.java
@@ -4,6 +4,7 @@ import com.himanshu.quizapp.Question;
 import com.himanshu.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,15 @@ public class QuestionController {
     @GetMapping("allQuestions")
     public List<Question> getAllQuestions(){
       return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQustionByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
+
+
+
+
     }
 
 }
--- src/main/java/com/himanshu/quizapp/dao/QuestionDao.java
+++ src/main/java/com/himanshu/quizapp/dao/QuestionDao.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Repository
 public interface QuestionDao extends JpaRepository<Question,Integer> {
 
+    List<Question> findByCategory(String category);
 
 
 
